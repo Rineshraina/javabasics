dd857ce8b01e00c49bd89b6229650e82ea308dbf
Rineshraina <rineshravi97@gmail.com>
2018-05-29 15:14:30 +0530

    DataTypeExample

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main(String[]args) {
+		String firstName="rinesh";
+		String lastName="raina";
+		System.out.println(firstName+""+lastName);
+	}
+
+}
