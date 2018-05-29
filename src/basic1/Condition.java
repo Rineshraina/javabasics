b69a14b18ba4580ed38f5ecabe435e2b635e6bf9
Rineshraina <rineshravi97@gmail.com>
2018-05-29 17:38:52 +0530

    Condition

---
+++ src/basic1/Condition.java
@@ -0,0 +1,15 @@
+package basic1;
+
+public class Condition {
+ public static void main(String[]args) {
+	 int a=10,b=15;
+	 if(a>b)
+	 {
+		 System.out.println(a+"\tis greater than\t"+b);
+	 }
+	 else
+	 {
+		 System.out.println(a+"\tis less than\t" +b);
+	 }
+ }
+}
