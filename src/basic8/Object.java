4c32776fd76add9163ae778f8946c59d244f5e05
Rineshraina <rineshravi97@gmail.com>
2018-05-30 18:07:28 +0530

    thirteen2

--- src/basic8/Object.java
+++ src/basic8/Object.java
@@ -1,15 +1,33 @@
 package basic8;
 
 public class Object {
-String model;
-double speed;
+private String model;
+private double speed;
 public Object() {
 	
 }
+ 
+public String getModel() {
+	return model;
+}
+
+public void setModel(String model) {
+	this.model = model;
+}
+
+public double getSpeed() {
+	return speed;
+}
+
+public void setSpeed(double speed) {
+	this.speed = speed;
+}
+
 public boolean start() {
 	return true;
 }
 public void accelerate() {
+	speed+=10;
 	
 }
 public static void main(String[]args) {
