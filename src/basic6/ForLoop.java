083e0073516ef96a2b5524d0d00df7f749928fc9
Rineshraina <rineshravi97@gmail.com>
2018-05-30 10:39:17 +0530

    ForLoop

---
+++ src/basic6/ForLoop.java
@@ -0,0 +1,17 @@
+package basic6;
+
+public class ForLoop {
+	public static void main( String[] args )
+	{
+	int count = 1,i;
+
+	for ( i=10;count <= i;++count )
+	{
+	 System.out.println( count % 2 == 1 ? "****" : "++++++++" );
+	 
+	 } 
+	 } 
+	 } 
+
+
+
