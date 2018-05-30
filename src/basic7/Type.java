be0d0b5b05ce7a1099c3e51407c13892a165c59b
Rineshraina <rineshravi97@gmail.com>
2018-05-30 12:12:37 +0530

    Typecast

---
+++ src/basic7/Type.java
@@ -0,0 +1,11 @@
+package basic7;
+
+public class Type {
+	public static void main(String[]args) {
+		byte one=100;
+		byte two=20;
+		byte three=(byte)(one+two);
+		System.out.println(three);
+	}
+
+}
