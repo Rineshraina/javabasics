c323e52fcd4e958ce0fff136b51ce3a67029e7e7
Rineshraina <rineshravi97@gmail.com>
2018-06-01 15:18:11 +0530

    Reverse

---
+++ src/basic/Arr.java
@@ -0,0 +1,18 @@
+package basic;
+
+public class Arr {
+	public static void main(String[] args) {
+		String name[]= {"raina","dhoni","billings","watson","bravo","jadeja"};
+		for (int i = 0; i < name.length; i++) {
+			System.out.println(name[i]);
+		}
+			int num[]=new int[5];
+			
+			for (int j = 0; j < num.length; j++) {
+				num[j]=j*3;
+				System.out.println(num[j]);
+			}
+		
+	}
+
+}
