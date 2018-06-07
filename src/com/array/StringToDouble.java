ce9302b397d35d4959afb5843fc4ec5c493913d6
Rineshraina <rineshravi97@gmail.com>
2018-06-07 14:25:08 +0530

    array

---
+++ src/com/array/StringToDouble.java
@@ -0,0 +1,14 @@
+package com.array;
+
+
+public class StringToDouble {
+	public static void main(String args[])
+	{
+	 
+		int s1 = Integer.parseInt(args[0]);	
+		int s2 = Integer.parseInt(args[1]);
+	int total;
+	total=s1+s2; 
+	System.out.printf("sum="+total);	
+	}
+}
