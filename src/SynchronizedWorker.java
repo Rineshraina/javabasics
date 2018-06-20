5b4ee3b5889ef810329765818a602b2329f732c1
Rineshraina <rineshravi97@gmail.com>
2018-06-20 12:26:27 +0530

    SynchronizedWorker

---
+++ src/SynchronizedWorker.java
@@ -0,0 +1,51 @@
+package com.java.util;
+
+public class SynchronizedWorker extends Thread {
+private int count=0;
+	public static void main(String[] args) {
+	SynchronizedWorker worker=new SynchronizedWorker();
+	worker.doWork();
+}
+public  void increment()throws InterruptedException{
+	
+	count++;
+}
+public void doWork() {
+	Thread thread1=new Thread(new Runnable() {
+		
+		public void run() {
+			for(int i=0;i<10000;i++) {
+				try {
+					increment();
+				}catch(InterruptedException ex) {
+					ex.printStackTrace();
+				}
+			}
+			
+		}
+	});
+	thread1.start();
+	
+Thread thread2=new Thread(new Runnable() {
+		
+		public void run() {
+			for(int i=0;i<10000;i++) {
+				try {
+					increment();
+				}catch(InterruptedException ex) {
+					ex.printStackTrace();
+				}
+			}
+			
+		}
+	});
+	thread2.start();
+	try {
+		thread1.join();
+		thread2.join();
+	}catch(InterruptedException ignored) {
+		System.out.println(count);
+	}
+	
+}
+}
